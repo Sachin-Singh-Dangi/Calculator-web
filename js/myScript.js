7ad1c768ac07c87b27261320378dcf77321a7b81
Sachin Singh Dangi <112658166+Sachin-Singh-Dangi@users.noreply.github.com>
2022-11-27 20:02:25 +0530

    Add files via upload

---
+++ js/myScript.js
@@ -0,0 +1,29 @@
+let string = "";
+let buttons = document.querySelectorAll(".button");
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML === "=") {
+      try {
+        string = eval(string);
+      } catch (error) {
+        alert(error);
+        string = "";
+      }
+      document.querySelector("input").value = string;
+    } else if (e.target.innerHTML === "+/-") {
+      var t = parseInt(string) * -1;
+      string = t;
+      document.querySelector("input").value = string;
+    } else if (e.target.innerHTML === "C") {
+      string = "";
+      document.querySelector("input").value = string;
+    } else if (e.target.innerHTML === "%") {
+      string = string / 100;
+      document.querySelector("input").value = string;
+    } else {
+      console.log(e.target);
+      string = string + e.target.innerHTML;
+      document.querySelector("input").value = string;
+    }
+  });
+});
